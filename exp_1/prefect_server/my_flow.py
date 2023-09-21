c4de607135d5201718f4d9c6590ecb20b76ec3d2
steffres <stefan.resch@oeaw.ac.at>
2023-09-21 11:38:39 +0200

    concurrency example1: task is sequential

--- exp_1/prefect_server/my_flow.py
+++ exp_1/prefect_server/my_flow.py
@@ -1,49 +1,41 @@
 import httpx
 from datetime import timedelta
-from prefect import flow, task, get_run_logger
+from prefect import flow, task
 from prefect.tasks import task_input_hash
 
 
 @task(cache_key_fn=task_input_hash, cache_expiration=timedelta(hours=1))
-def get_repo_info(repo_owner: str, repo_name: str):
-    """Get info about a repo - will retry twice after failing"""
-    print("RUNNING: get_repo_info")
-    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}"
-    api_response = httpx.get(url)
-    api_response.raise_for_status()
-    repo_info = api_response.json()
-    return repo_info
-
-
-@task
-def get_contributors(repo_info: dict):
-    contributors_url = repo_info["contributors_url"]
-    response = httpx.get(contributors_url)
+def get_url(url: str, params: dict = None):
+    response = httpx.get(url, params=params)
     response.raise_for_status()
-    contributors = response.json()
-    return contributors
+    return response.json()
 
 
-@flow(retries=3, retry_delay_seconds=5, name="Repo Info", log_prints=True)
-def repo_info(repo_owner: str = "PrefectHQ", repo_name: str = "prefect"):
-    """
-    Given a GitHub repository, logs the number of stargazers
-    and contributors for that repo.
-    """
-    logger = get_run_logger()
-    logger.info("################# VERSION 4 ##################")
-    #raise Exception("BIG FAT ERROR")
-    repo_info = get_repo_info(repo_owner, repo_name)
-    print(f"Stars 🌠 : {repo_info['stargazers_count']}")
+def get_open_issues(repo_name: str, open_issues_count: int, per_page: int = 100):
+    issues = []
+    pages = range(1, -(open_issues_count // -per_page) + 1)
+    for page in pages:
+        issues.append(
+            get_url(
+                f"https://api.github.com/repos/{repo_name}/issues",
+                params={"page": page, "per_page": per_page, "state": "open"},
+            )
+        )
+    return [i for p in issues for i in p]
 
-    contributors = get_contributors(repo_info)
-    print(f"Number of contributors 👷: {len(contributors)}")
+
+@flow(retries=3, retry_delay_seconds=5, log_prints=True)
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    repo_stats = get_url(f"https://api.github.com/repos/{repo_name}")
+    issues = get_open_issues(repo_name, 500, 10)
+    issues_per_user = len(issues) / len(set([i["user"]["id"] for i in issues]))
+    print(f"{repo_name} repository statistics 🤓:")
+    print(f"Stars 🌠 : {repo_stats['stargazers_count']}")
+    print(f"Forks 🍴 : {repo_stats['forks_count']}")
+    print(f"Average open issues per user 💌 : {issues_per_user:.2f}")
 
 
 if __name__ == "__main__":
     # run manually
-    repo_info()
-
-    # create your first deployment
-    #repo_info.serve(name="my-first-deployment V3")
+    get_repo_info()
 

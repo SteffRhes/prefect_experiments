b78d2b2a62ce4339a06921d657c208240fd89947
steffres <stefan.resch@oeaw.ac.at>
2023-09-25 12:43:09 +0200

    create example breaking with concurrency

---
+++ exp_1/prefect_server/break_concurrency_example.py
@@ -0,0 +1,29 @@
+import time
+from prefect import flow, task
+
+
+value = None
+
+@task()
+def bar(i):
+    global value
+    if i == 1:
+        value = 1
+        print("case 1, value:", value)
+        time.sleep(3)
+        print("case 1, value:", value)
+    elif i == 2:
+        value = 2
+        print("case 2, value:", value)
+
+
+@flow()
+def foo():
+    bar.submit(1)
+    bar.submit(2)
+    
+
+if __name__ == "__main__":
+    foo()
+
+
